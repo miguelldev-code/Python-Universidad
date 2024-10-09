6a47f2e1e484b58b8c1e1b0a733a4cc4299c091a
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-10-08 23:19:55 -0500

    input python

--- envioDatos.py
+++ envioDatos.py
@@ -1,2 +1,21 @@
 #  ENVIO DE DATOS CON INPUT
 
+resultado = input("Escribe un mensaje: ")
+print(resultado)
+print("Fin del programa")
+
+# CONCATENACION CON STRING 
+numeroUno = input("Escribe numero uno: ")
+numeroDos = input("Escribe numero dos: ")
+resultadoSuma = numeroUno + numeroDos
+print(resultadoSuma)
+print(type(resultadoSuma))  
+
+#CONCATENACION CON ENTEROS
+numeroUno = int(input("Escribe numero uno: "))
+numeroDos = int(input("Escribe numero dos: "))
+resultadoSuma = numeroUno + numeroDos
+print(resultadoSuma)  
+print(type(resultadoSuma))  
+
+
