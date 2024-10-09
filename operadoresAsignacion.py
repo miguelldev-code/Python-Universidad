d7580246594c96b4eec1f08d7d4edd874cbcae13
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-10-09 13:08:41 -0500

    operadores python

---
+++ operadoresAsignacion.py
@@ -0,0 +1,19 @@
+numero = 3
+print(numero)
+
+# INCREMENTO 
+numero = numero + 1
+print(numero) # 4   
+
+# INCREMENTO CON REASIGNACION
+numero += 1
+print(numero) # 5
+
+numero -= 2
+print(numero) # 3
+
+numero *= 3
+print(numero) # 9
+
+numero /= 3
+print(numero) # 3.0 float 
---
+++ operadoresComparacion.py
@@ -0,0 +1,26 @@
+a = 2
+b = 2
+print("a = 2\nb = 4")
+print("resultado = a x b")
+print("x es operador utilizado\n")
+
+# OPERADOR ==
+resultado = a == b # True
+print(f"Resultado con ==: {resultado}")
+
+# OPERADOR !=
+resultado = a != b # False
+print(f"Resultado con !=: {resultado}")
+
+# OPERADOS > o <
+resultado = a > b # False
+print(f"Resultado con >: {resultado}")
+resultado = a < b # False
+print(f"Resultado con <: {resultado}")
+
+# OPERADOS > o <
+resultado = a >= b # True
+print(f"Resultado con >: {resultado}")
+resultado = a <= b # True
+print(f"Resultado con <: {resultado}")
+
