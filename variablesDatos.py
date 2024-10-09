3855750c7c559c350c9d0b41d20c6dd74e62f490
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-10-09 12:34:56 -0500

    variables Datos python

---
+++ variablesDatos.py
@@ -0,0 +1,24 @@
+# VARIABLES ENTERAS
+
+valorUno = 3
+valorDos = 2
+
+suma= valorUno + valorDos
+resta = valorUno - valorDos
+multipliacion = valorUno * valorDos
+division = valorUno / valorDos
+divisionEntera = valorUno // valorDos
+modulo = valorUno % valorDos
+potencia = valorUno ** valorDos
+
+#   IMPRESION COMUN
+# print("Resultado suma: ", suma)
+
+#   INTERPOLACION
+print(f"Resultado suma: {suma}")
+print(f"Resultado resta: {resta}")
+print(f"Resultado multiplicacion: {multipliacion}") 
+print(f"Resultado division: {division}")
+print(f"Resultado division entera: {divisionEntera}")
+print(f"Resultado modulo: {modulo}")
+print(f"Resultado potencia: {potencia}")
