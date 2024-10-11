ead5a548700c7ef08d3140a1d8af03dcfa1f8c14
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-10-11 13:51:27 -0500

    estructurar repositorio python

--- condicionales/ejercicioEstaciones.py
+++ condicionales/ejercicioEstaciones.py
@@ -1,5 +1,6 @@
-mes = int(input("Ingresa el mes: "))
+mes = int(input("Ingresa el mes (1 a 12): "))
 estacion = None
+
 
 if mes == 1 or mes == 2 or mes == 12:
     estacion = "Invierno"
@@ -9,5 +10,6 @@ elif mes == 6 or mes == 7 or mes == 8:
     estacion = "Verano"
 elif mes == 9 or mes == 10  or mes == 11:
     estacion = "Otoño"
-
-print(f"En el mes {mes}, la estacion es {estacion}")
+else:
+    print("El mes ingresado es incorrecto")
+print(f"En el mes {mes}, la estacion es {estacion}")    
