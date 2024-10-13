d22449af6c848f465ea35bbc6f1efe5db13b5f30
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-10-12 19:43:52 -0500

    condicionales python

---
+++ condicionales/for.py
@@ -0,0 +1,7 @@
+cadena = "Texto separado"
+
+for letra in cadena:
+    print(letra)
+
+else:
+    print("Fin del ciclo") 
---
+++ condicionales/while.py
@@ -0,0 +1,13 @@
+# condicion = True
+# while condicion:
+#     print("Condicion verdadera")
+# else:
+#     print("Falsooo")
+
+contador = 0
+while contador <= 3:
+    print(contador)
+    contador += 1
+else:
+    print(contador, "Fuera del ciclo")
+
