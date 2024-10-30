0c25155f741b81947b519315acffa7f32909ad5f
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-10-29 22:08:17 -0500

    mejorar comentarios diccionarios pyton

--- coleccionesDiccionarios/Diccionario.py
+++ coleccionesDiccionarios/Diccionario.py
@@ -1,6 +1,9 @@
-# Par clave-valor: Cada elemento tiene dos partes: una clave y un valor. Por ejemplo, en el par nombre: "Juan", "nombre" es la clave y "Juan" es el valor.
+# Par clave-valor: Cada elemento tiene dos partes: una clave y un valor. 
+# Por ejemplo, en el par nombre: "Juan", "nombre" es la clave y "Juan" es el valor.
 
-# Estructura de un Diccionario: Los diccionarios se definen usando llaves {}. Dentro, los pares clave-valor están separados por comas, y cada clave se separa de su valor con dos puntos :.
+# Estructura de un Diccionario: Los diccionarios se definen usando llaves {}.
+#  Dentro, los pares clave-valor están separados por comas, y cada clave se 
+# separa de su valor con dos puntos :.
 
 estudiante = {
     "nombre": "Juan",
@@ -9,10 +12,10 @@ estudiante = {
 }
 
 
-# structura de un Diccionario: Los diccionarios se definen usando llaves {}. Dentro, los pares clave-valor están separados por comas, y cada clave se separa de su valor con dos puntos :.
 print(estudiante["nombre"])  # Output: Juan
 
-#   Acceso a Valores: Puedes acceder al valor de un diccionario usando la clave correspondiente entre corchetes [].
+#   Acceso a Valores: Puedes acceder al valor de un diccionario usando la clave 
+# correspondiente entre corchetes [].
 
 print(estudiante.get("promedio", "No especificado"))  # Output: No especificado
 
