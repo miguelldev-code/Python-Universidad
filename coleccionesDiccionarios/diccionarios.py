279bcd8528216f3fbcec428fe1e842796f4094dc
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-11-02 19:42:50 -0500

    Finaliazacion ejercicios diccionarios Python

--- coleccionesDiccionarios/diccionarios.py
+++ coleccionesDiccionarios/diccionarios.py
@@ -1,14 +1,28 @@
 # SINTAXIS
  
-
+#Conjuntos o set
+# Mutables por medio de metodos
+# add, remove, union 
+# NO SE ACCEDE POR INDICE
+# No tiene posiciones fijas
 usuario = ("Juan", "juan", "1234")
 print(type(usuario))
 
 # permiten estructurar y organizar la información de forma lógica
 
-
+# Diccionarios
+# Mutables por medio de metodos
+# NO SE ACCEDE POR INDICE, sino por valor
+# No tiene orden especifico
 variable = {"Clave":"Valor de la clave"}
 print(type(variable))
 print(variable)
 
 
+# Nos permite hacer:
+
+#Almacenar Datos Relacionados con Etiquetas
+#Acceso Directo a Información Específica
+#Realizar Búsquedas Rápidas y Eficientes
+#Organizar Datos de Configuración
+#Relacionar Objetos Complejos en Bases de Datos Simples
