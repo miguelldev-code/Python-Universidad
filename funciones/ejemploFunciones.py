7937645416096c982b4b44183470aab657b2dc0b
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-11-13 00:03:21 -0500

    Ejemplo de funciones en python

---
+++ funciones/ejemploFunciones.py
@@ -0,0 +1,16 @@
+# Parámetro con valor por defecto
+def saludar(nombre="Usuario"):
+    print(f"Hola, {nombre}!")
+
+saludar()  # Imprime: Hola, Usuario!
+saludar("Carlos")  # Imprime: Hola, Carlos!
+
+# Uso de *args y **kwargs
+def mostrar_datos(*args, **kwargs):
+    print("Argumentos posicionales:", args)
+    print("Argumentos nombrados:", kwargs)
+
+mostrar_datos(1, 2, 3, nombre="Ana", edad=30)
+# Imprime:
+# Argumentos posicionales: (1, 2, 3)
+# Argumentos nombrados: {'nombre': 'Ana', 'edad': 30}
