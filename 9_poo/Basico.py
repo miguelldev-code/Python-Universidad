5fb751f6f107f8aea0eeb6b21dc6d5316f85317a
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-09-10 12:18:25 -0500

    Creando clase producto

--- 9_poo/Basico.py
+++ 9_poo/Basico.py
@@ -1,13 +1,32 @@
 class Perro:
     # Constructor (inicializa atributos)
-    def __init__(self, nombre, edad):
-        self.nombre = nombre  # Atributo de instancia
-        self.edad = edad
+    def __init__(mascota, nombre, edad):
+        mascota.nombre = nombre  # Atributo de instancia
+        mascota.edad = edad
 
     # Método
-    def ladrar(self):
-        print(f"{self.nombre} dice: ¡Guau!")
+    def ladrar(mascota):
+        print(f"{mascota.nombre} dice: ¡Guau!")
 
 # Crear objeto
 mi_perro = Perro("Rex", 3)
 mi_perro.ladrar()  # Salida: Rex dice: ¡Guau!
+
+
+class Producto:
+    def __init__(self, descripcion, cantidad):
+        self.descripcion = descripcion
+        self.cantidad= cantidad
+
+    def mostrar_info(self):
+        print(f"Producto: {self.descripcion}, Cantidad: {self.cantidad}")
+
+    def stock(self):
+        return self.cantidad <= 3
+    
+producto1 = Producto("Jabon", 20)
+producto2 = Producto("Shampoo", 2)
+
+print(producto1.descripcion)
+print(producto1.stock())
+print(producto2.stock())
